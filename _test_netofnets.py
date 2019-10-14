a746dcc348c6e7dad16ca5c7c616d0521e39725a
giosumarin <giosumarin@yahoo.com>
2019-10-14 13:07:48 +0200

    upgrade for make_split with each number of split, set same random value for each run test

--- _test_netofnets.py
+++ _test_netofnets.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 import h5py
 import time 
-from math import floor, sqrt
+from math import floor, sqrt, ceil
 
 
 from NN_no_hidden import NN as NN1
@@ -16,13 +16,12 @@ from NN_pr import WS_module as ws
 def make_structured_input_for_root_NN(bin_data, labels, split, dim_set):
     position_labels = np.copy(labels)
     size_batch = dim_set // split
-    #if dim_set % split == 0: 
+    remain = ceil((dim_set / split - size_batch) * split)
     for i in range(0, split, 1):
-        position_labels[(i*size_batch):((i+1)*size_batch)]=i
-    #position_labels[result>split-1] = split-1
+        position_labels[(i*size_batch):((i+1)*(size_batch if i < (split-1) else size_batch+remain))]=i
     position_labels = np.reshape(position_labels, (-1,1))
-    splitted_labels = [np.reshape(labels[(i*size_batch):((i+1)*size_batch)]/(dim_set/split), (-1,1)) for i in range(split)]
-    splitted_bin_data = [bin_data[(i*size_batch):((i+1)*size_batch),:] for i in range(split)] 
+    splitted_labels = [np.reshape(labels[(i*size_batch):((i+1)*(size_batch if i < (split-1) else size_batch+remain))]/dim_set, (-1,1)) for i in range(split)]
+    splitted_bin_data = [bin_data[(i*size_batch):((i+1)*(size_batch if i < (split-1) else size_batch+remain)),:] for i in range(split)] 
 
     p = np.random.RandomState(seed=42).permutation(dim_set)
     p_split = np.random.RandomState(seed=42).permutation(dim_set//split)
@@ -33,48 +32,14 @@ def make_structured_input_for_root_NN(bin_data, labels, split, dim_set):
 
 
     return bin_data[p], perm_splitted_bin_data, perm_position_labels, perm_splitted_labels
-            #sistema ultimo
-
-# def make_bin_tree(bin_data, labels, dim_set, layers):
-#     position_labels = np.copy(labels)
-#     split = 2
-#     size_batch = dim_set // split
-#     size_batch = [dim_set // (split * l) for l in range(1, layers, 1)]
-#     mul_for_layers = 
-#     pos_labels_list = []
-#     #if dim_set % split == 0:
-#     for l in range (1,layers,1):
-#         for i in range (split):
-#             copy = np.copy(labels)
-#         pos_labels_list[l] = [position_labels[i*size_batch[l-1]:(i+1)*size_batch[l-1]]=i for i in range(split*l)]
-#         pos_labels_list.append([copy[i*size_batch[l-1]:(i+1)*size_batch[l-1]] = i for i in range(split*l)])
-        
-
-
-
-
-#     pos_labels_list[1] = [position_labels[(i*size_batch[1]:(i+1)*size_batch[1])]=i for i in range(split*l]
-
-
-
-
-    # #for l in range(layers-1):
-    # for l in range(1, layers, 1):
-    #     pos_labels_list[l] = [position_labels[(i*size_batch[l]):((i+1))*size_batch[l])]=i for i in range(0, split, 1)]
-    # #position_labels[result>split-1] = split-1
-    # position_labels = np.reshape(position_labels, (-1,1))
-    # splitted_labels = [np.reshape(labels[(i*size_batch):((i+1)*size_batch)]/(dim_set/split), (-1,1)) for i in range(split)]
-    # splitted_bin_data = [bin_data[(i*size_batch):((i+1)*size_batch),:] for i in range(split)] 
-
-    # p = np.random.RandomState(seed=42).permutation(dim_set)
-    # p_split = np.random.RandomState(seed=42).permutation(dim_set//split)
-
-    # perm_splitted_bin_data = [splitted_bin_data[i][p_split] for i in range(split)]
-    # perm_position_labels = position_labels[p]
-    # perm_splitted_labels = [splitted_labels[i][p_split] for i in range(split)]
-
-
-    # return bin_data[p], perm_splitted_bin_data, perm_position_labels, perm_splitted_labels
+    
+    
+def make_labels_for_class(position_labels):
+    n_rows = position_labels.shape[0]
+    result = np.zeros((n_rows,2))
+    for i in range(n_rows):
+        result[i,position_labels[i]] = 1
+    return result
 
 
 N_FEATURES = 64
@@ -83,13 +48,16 @@ np.random.RandomState(42)
 
 weights = np.random.randn(N_FEATURES, N_CLASSES).astype(np.float32) * sqrt(2/N_FEATURES)
 #weights = (np.zeros((N_FEATURES, N_CLASSES))).astype(np.float32)
-bias = np.ones((1, N_CLASSES)).astype(np.float32)*0.001
+bias = np.ones((1, N_CLASSES)).astype(np.float32)*0.0001
 #bias = np.random.randn(1, N_CLASSES).astype(np.float32) * sqrt(2/N_CLASSES)
 w= [[weights, bias]]
 
 
-for spl in [2]:
-    for i in [10]:
+
+for i in [3,7,10]:
+    with open("to_tex.txt", "a+") as tex:
+            tex.write("\nfile {}\n".format(i))
+    for spl in [2,3,4,5,6,7,8]:
         with h5py.File('Resource2/file'+str(i)+'uniform_bin.sorted.mat','r') as f:
             data = f.get('Sb') 
             bin_data = np.array(data, dtype=np.bool)
@@ -97,49 +65,92 @@ for spl in [2]:
             bin_data = np.flip(bin_data,axis=1)
             dim_set = len(bin_data)
         
-            split = spl
-            labels = np.linspace(1, len(bin_data), num=len(bin_data), dtype=np.float64)
-            bin_data, splitted_bin_data, position_labels, splitted_labels = make_structured_input_for_root_NN(bin_data, labels, split, dim_set)
+        split = spl
+        labels = np.linspace(1, len(bin_data), num=len(bin_data), dtype=np.float64)
+        bin_data, splitted_bin_data, position_labels, splitted_labels = make_structured_input_for_root_NN(bin_data, labels, split, dim_set)
+
+        # for i in range(split): 
+        #     print(np.max(splitted_labels[i]))
+        #     print(np.min(splitted_labels[i]))
+
+        # labels = labels/len(bin_data)
+        # labels = np.reshape(labels, (-1, 1))
+        # bin_data = bin_data[p]
+        # labels = labels[p]
+
+        # position_labels_class = make_labels_for_class(position_labels.astype('int8'))
+        # nn = NN1.NN(training=[bin_data, position_labels_class], testing=[[0],[0]], lr=0.03, mu=0.9, output_classes=2, minibatch=32, disableLog=True)
+        # nn.addLayers(['softmax'])
+        # now = time.time()
+        # loss = nn.train(stop_function=4, num_epochs=20000)
+        # diff = time.time() - now
+        # res = np.argmax(nn.predict(bin_data),axis=1).reshape(-1,1)
+        # sum = np.sum(np.abs(res-position_labels))
+        # print("epoch = {} - loss on sel_brancher = {} - sum error = {} - time = {}s".format(nn.epoch, loss, sum, round(diff,5)))
         
-        #labels = labels/len(bin_data)
-        #labels = np.reshape(labels, (-1, 1))
-        #bin_data = bin_data[p]
-        #labels = labels[p]
         
-        nn = NN1.NN(training=[bin_data, position_labels], testing=[[0],[0]], lr=0.1, mu=0.9, lambd=0, minibatch=32, disableLog=True)
-        nn.addLayers(['sigmoid'])
-        now = time.time()
-        loss = nn.train(stop_function=4, num_epochs=20000)
-        diff = time.time() - now
-        res = nn.predict(bin_data)
-        res[res <= 0.5]=0
-        res[res > 0.5]=1
-        sum = np.sum(np.abs(res-position_labels))
-        print("epoch = {} - loss on sel_brancher = {} - sum error = {} - time = {}s".format(nn.epoch, loss, sum, round(diff,5)))
+        
+        
+        # position_labels_class = make_labels_for_class(position_labels.astype('int8'))
+        # nn = NN.NN(training=[bin_data, position_labels_class], testing=[[0],[0]], lr=0.03, mu=0.9, output_classes=2, minibatch=32, disableLog=True)
+        # nn.addLayers([32],['relu', 'softmax'])
+        # now = time.time()
+        # loss = nn.train(stop_function=4, num_epochs=100)
+        # diff = time.time() - now
+        # res = np.argmax(nn.predict(bin_data),axis=1).reshape(-1,1)
+        # sum = np.sum(np.abs(res-position_labels))
+        # print("H: epoch = {} - loss on sel_brancher = {} - sum error = {} - time = {}s".format(nn.epoch, loss, sum, round(diff,5)))
+
+
+        #H: epoch = 45 - loss on sel_brancher = 9.1e-06 - sum error = 0.0 - time = 462.80945s
+        
+        
+        #nn = NN1.NN(training=[bin_data, position_labels], testing=[[0],[0]], lr=0.1, mu=0.9, lambd=0, minibatch=32, disableLog=True)
+        #nn.addLayers(['sigmoid'])
+        #now = time.time()
+        #loss = nn.train(stop_function=4, num_epochs=20000)
+        #diff = time.time() - now
+        #res = nn.predict(bin_data)
+        #res[res <= 0.5]=0
+        #res[res > 0.5]=1
+        #sum = np.sum(np.abs(res-position_labels))
+        #print("epoch = {} - loss on sel_brancher = {} - sum error = {} - time = {}s".format(nn.epoch, loss, sum, round(diff,5)))
         
         '''
         f3: epoch = 1013 - loss on sel_brancher = 0.0237729 - sum error = 0.0 - time = 0.97598s
         f7: epoch = 12679 - loss on sel_brancher = 0.0024501 - sum error = 0.0 - time = 176.93983s
         '''
-
+        max_errs = []
         for s in range(split):
-            nn = NN1.NN(training=[splitted_bin_data[s], splitted_labels[s]], testing=[[0],[0]], lr=0.01, mu=0.9, lambd=0, minibatch=32, disableLog=True)
-            nn.addLayers(['leakyrelu'])
+            ww = np.copy(w)
+            nn = NN1.NN(training=[splitted_bin_data[s], splitted_labels[s]], testing=[[0],[0]], lr=0.05, mu=0.9, output_classes=1, lambd=1e-5, minibatch=32, disableLog=True)
+            nn.addLayers(['leakyrelu'], ww)
             now=time.time()
             loss = nn.train(stop_function=3, num_epochs=20000)
             difference = round(time.time() - now, 5)
-            max_err=0
-            mean_err=0
-            for j in range(dim_set // split):
-                pr = floor(nn.predict(splitted_bin_data[s][j])[0]*(dim_set//split))
-                val=abs(pr-splitted_labels[s][j]*(dim_set//split))
-                if val > max_err:
-                    max_err = val
-                mean_err += val
-            mean_err/=dim_set
+ 
+           
             
+            # max_err=0
+            # mean_err=0
+            # for j in range(dim_set // split) :
+            #     pr = floor(nn.predict(splitted_bin_data[s][j])[0]*(dim_set))
+            #     val=abs(pr-splitted_labels[s][j]*(dim_set))
+            #     if val > max_err:
+            #         max_err = val
+            #     mean_err += val
+            # mean_err/=dim_set
+            
+            pr = np.floor(nn.predict(splitted_bin_data[s]) * dim_set)
+            lab = splitted_labels[s] *dim_set
+            max_err = np.max(np.abs(pr-lab)).astype("int32")
+            max_errs.append(max_err)
             print("0 hidden --> file {}, split={}, dim={}: epoch: {} -- maxerr={} -- %err={} -- meanErr={} -- time={}s -- spaceOVH={}"
-            .format(i, spl, (dim_set/split), nn.epoch, max_err[0], round(max_err[0]/(dim_set/split)*100,3), round(loss, 5), difference, round(nn.get_memory_usage(),5)))
+            .format(i, spl, (dim_set/split), nn.epoch, max_err, round(max_err/(dim_set)*100,3), round(loss, 5), difference, round(nn.get_memory_usage(),5)))
+        
+        with open("to_tex.txt", "a+") as tex:
+            tex.write("${}$ & ${}$ & ${}$ & ${}$ \\\ \n".format(spl, list(max_errs), max(max_errs), round(nn.get_memory_usage()*spl,5)))
+        
 
 
 
