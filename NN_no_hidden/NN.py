correct formula of space OVH and add more option for number of neuron (hidden layer)

NN_no_hidden/NN.py
@@ -60,6 +60,9 @@ class NN:
     def set_lambda_reg_l2(self, lambd):
         self.lambd=lambd
 
+    def set_patience(self, patience):
+        self.patience = patience
+
     def feedforward(self, X):
         inputLayer = X            
         return [self.act_fun[0](np.dot(inputLayer, self.layers[0][0]) + self.layers[0][1], False)]
@@ -83,12 +86,12 @@ class NN:
                 indexLow = n * tr
                 indexHigh =(n+1) * tr 
                 predictions = self.predict(X[indexLow:indexHigh])
-                loss += np.mean(np.abs(predictions - t[indexLow:indexHigh]))
+                loss += np.mean(np.square(predictions - t[indexLow:indexHigh]))
                 
             loss/=batch
         else:
             predictions = self.predict(X)
-            loss = np.mean(np.square(predictions-t)) #np.abs
+            loss = np.mean(np.square(predictions-t))
         return np.round(loss, 7)
 
 

NN_pr/NN.py
@@ -106,7 +106,7 @@ class NN:
             loss/=batch
         else:
             predictions = self.predict(X)
-            loss = np.mean(np.abs(predictions-t)) 
+            loss = np.mean(np.square(predictions-t)) 
         return round(loss, 7)
 
 

_test_netofnets_1hidden.py
@@ -53,7 +53,7 @@ np.random.RandomState(0)
 # weights2 = np.random.randn(16, N_CLASSES).astype(np.float32) * sqrt(2/N_FEATURES)
 # bias2 = np.ones((1, N_CLASSES)).astype(np.float32)*0.001
 # wh= [[weights1, bias1], [weights2, bias2]]
-for size in [8, 16, 32, 64]:
+for size in [8, 16, 32, 64, 128]:
     weights1 = np.random.randn(N_FEATURES, size).astype(np.float32) * sqrt(2/N_FEATURES)
     bias1 = np.ones((1, size)).astype(np.float32)*0.001
 
@@ -97,20 +97,20 @@ for size in [8, 16, 32, 64]:
                 scaler = MinMaxScaler()
                 transformed_lab = scaler.fit_transform(splitted_labels[s])
 
-                nn = NN.NN(training=[splitted_bin_data[s], transformed_lab], testing=[[0],[0]], lr=0.05, mu=0.9, output_classes=1, lambd=0, minibatch=32, disableLog=True)
+                nn = NN.NN(training=[splitted_bin_data[s], transformed_lab], testing=[[0],[0]], lr=0.03, mu=0.9, output_classes=1, lambd=0, minibatch=32, disableLog=True)
                 nn.addLayers([size],['leakyrelu','leakyrelu'], ww)
                 nn.set_patience(10)
-                now=time.time()
+                now= time.time()
                 loss = nn.train(stop_function=3, num_epochs=10000)
                 difference = round(time.time() - now, 3)
                 pr = np.floor(scaler.inverse_transform(nn.predict(splitted_bin_data[s])) * dim_set)
-                lab = splitted_labels[s] *dim_set
+                lab = splitted_labels[s] * dim_set
                 max_err = np.max(np.abs(pr-lab)).astype("int32")
                 max_errs.append(max_err)
                 
                 
                 print("1 hidden --> file {}, split={}, dim={}: epoch: {} -- maxerr={} -- %err={} -- meanErr={} -- time={}s -- spaceOVH={}"
-                .format(i, spl, dim_set, nn.epoch, max_err, round(max_err/(dim_set)*100,3), round(loss, 5), difference, round(nn.get_memory_usage(dim_set),5)))
+                .format(i, spl, dim_set/split, nn.epoch, max_err, round(max_err/(dim_set)*100,3), round(loss, 5), difference, round(nn.get_memory_usage(dim_set),5)))
             
             
             with open("to_tex_h1.txt", "a+") as tex:

_test_netofnets_nohidden.py
@@ -5,6 +5,7 @@ import sys
 import h5py
 import time 
 from math import floor, sqrt, ceil
+from sklearn.preprocessing import MinMaxScaler
 
 
 from NN_no_hidden import NN as NN1
@@ -81,21 +82,25 @@ for i in [3,7,10]:
         max_errs = []
         for s in range(split):
             ww = np.copy(w)
+            scaler = MinMaxScaler()
+            transformed_lab = scaler.fit_transform(splitted_labels[s])
             nn = NN1.NN(training=[splitted_bin_data[s], splitted_labels[s]], testing=[[0],[0]], lr=0.03, mu=0.9, output_classes=1, lambd=0, minibatch=32, disableLog=True)
             nn.addLayers(['leakyrelu'], ww)
+            nn.set_patience(10)
             now=time.time()
             loss = nn.train(stop_function=3, num_epochs=20000)
             difference = round(time.time() - now, 5)
             
+            #pr = np.floor(scaler.inverse_transform(nn.predict(splitted_bin_data[s])) * dim_set)
             pr = np.floor(nn.predict(splitted_bin_data[s]) * dim_set)
             lab = splitted_labels[s] *dim_set
             max_err = np.max(np.abs(pr-lab)).astype("int32")
             max_errs.append(max_err)
             print("0 hidden --> file {}, split={}, dim={}: epoch: {} -- maxerr={} -- %err={} -- meanErr={} -- time={}s -- spaceOVH={}"
-            .format(i, spl, (dim_set/split), nn.epoch, max_err, round(max_err/(dim_set)*100,3), round(loss, 5), difference, round(nn.get_memory_usage(),5)))
+            .format(i, spl, ceil(dim_set/split), nn.epoch, max_err, round(max_err/(dim_set)*100,3), round(loss, 5), difference, round(nn.get_memory_usage(dim_set),5)))
         
         with open("to_tex.txt", "a+") as tex:
-            tex.write("${}$ & ${}$ & ${}$ & ${}$ \\\ \n".format(spl, list(max_errs), max(max_errs), round(nn.get_memory_usage()*spl,5)))
+            tex.write("${}$ & ${}$ & ${}$ & ${}$ \\\ \n".format(spl, list(max_errs), max(max_errs), round(nn.get_memory_usage(dim_set)*spl,5)))
 
         print("-*-*"*35)
 