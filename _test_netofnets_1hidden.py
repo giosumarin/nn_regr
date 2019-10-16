b7bc71a19ec885bd36ee3a0ee3be28b39d7aeb9a
giosumarin <giosumarin@yahoo.com>
2019-10-16 12:05:52 +0200

    add also only 1 hidden layer

--- _test_netofnets_1hidden.py
+++ _test_netofnets_1hidden.py
@@ -46,14 +46,62 @@ def make_labels_for_class(position_labels):
 
 N_FEATURES = 64
 N_CLASSES = 1
-np.random.RandomState(0)
 
-# weights1 = np.random.randn(N_FEATURES, 16).astype(np.float32) * sqrt(2/N_FEATURES)
-# bias1 = np.ones((1, 16)).astype(np.float32)*0.001
+for size in [8, 16, 32, 64, 128]:
+    np.random.RandomState(0)
+    weights1 = np.random.randn(N_FEATURES, size).astype(np.float32) * sqrt(2/N_FEATURES)
+    bias1 = np.ones((1, size)).astype(np.float32)*0.001
 
-# weights2 = np.random.randn(16, N_CLASSES).astype(np.float32) * sqrt(2/N_FEATURES)
-# bias2 = np.ones((1, N_CLASSES)).astype(np.float32)*0.001
-# wh= [[weights1, bias1], [weights2, bias2]]
+    weights2 = np.random.randn(size, N_CLASSES).astype(np.float32) * sqrt(2/N_FEATURES)
+    bias2 = np.ones((1, N_CLASSES)).astype(np.float32)*0.001
+    wh= [[weights1, bias1], [weights2, bias2]]
+
+    for i in [3,7,10]:
+            with open("to_tex_h0.txt", "a+") as tex:
+                    tex.write("\nfile {} size_hidden {}\n".format(i, size))
+            for spl in [2,4,8,16]:
+                with h5py.File('Resource2/file'+str(i)+'uniform_bin.sorted.mat','r') as f:
+                    data = f.get('Sb') 
+                    bin_data = np.array(data, dtype=np.bool)
+                    bin_data = np.transpose(bin_data)
+                    bin_data = np.flip(bin_data,axis=1)
+                    dim_set = len(bin_data)
+                
+                split = spl
+                labels = np.linspace(1, len(bin_data), num=len(bin_data), dtype=np.float64)
+                bin_data, splitted_bin_data, position_labels, splitted_labels = make_structured_input_for_root_NN(bin_data, labels, split, dim_set)
+
+
+                max_errs = []
+                for s in range(split):
+                    ww = np.copy(wh)
+                    scaler = MinMaxScaler()
+                    transformed_lab = scaler.fit_transform(splitted_labels[s])
+
+                    nn = NN.NN(training=[splitted_bin_data[s], transformed_lab], testing=[[0],[0]], lr=0.05, mu=0.9, output_classes=1, lambd=0, minibatch=32, disableLog=True)
+                    nn.addLayers([size],['leakyrelu','leakyrelu'], ww)
+                    nn.set_patience(5)
+                    now= time.time()
+                    loss = nn.train(stop_function=3, num_epochs=20000)
+                    difference = round(time.time() - now, 3)
+                    pr = np.floor(scaler.inverse_transform(nn.predict(splitted_bin_data[s])) * dim_set)
+                    lab = splitted_labels[s] * dim_set
+                    max_err = np.max(np.abs(pr-lab)).astype("int64")
+                    max_errs.append(max_err)
+                    
+                    
+                    print("1 hidden --> file {}, split={}, dim={}: epoch: {} -- maxerr={} -- %err={} -- meanErr={} -- time={}s -- spaceOVH={}"
+                    .format(i, spl, dim_set/split, nn.epoch, max_err, round(max_err/(dim_set)*100,3), round(loss, 5), difference, round(nn.get_memory_usage(dim_set),5)))
+                
+                
+                with open("to_tex_h0.txt", "a+") as tex:
+                    tex.write("${}$ & ${}$ & ${}$ & ${}$ \\\ \n".format(spl, list(max_errs), max(max_errs), round(nn.get_memory_usage(dim_set)*spl,5)))
+                print("-*-*"*35)
+
+
+
+
+
 for size1,size2 in product([8, 16, 32, 64],[8, 16, 32, 64]):
     if size1 >= size2:
         # weights1 = np.random.randn(N_FEATURES, size).astype(np.float32) * sqrt(2/N_FEATURES)
